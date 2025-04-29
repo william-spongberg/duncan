d9808765f548868a76d82b12816ddf66c5d84e1c
William Spongberg <william.spongberg@outlook.com>
2025-04-29 17:04:46 +1000

    Fix manifest.json

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,6 @@ import { type NextRequest } from 'next/server'
 export async function middleware(request: NextRequest) {
   return await updateSession(request)
 }
-
 export const config = {
   matcher: [
     /*
@@ -12,8 +11,9 @@ export const config = {
      * - _next/static (static files)
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
+     * - manifest.json (web app manifest)
      * Feel free to modify this pattern to include more paths.
      */
-    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+    '/((?!_next/static|_next/image|favicon.ico|manifest.json|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
   ],
 }
