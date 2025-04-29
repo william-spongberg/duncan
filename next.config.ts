9df191da38634e9e45aee4b0f4e7505a5affe92a
William Spongberg <william.spongberg@outlook.com>
2025-04-29 16:30:27 +1000

    Add icons

--- next.config.ts
+++ next.config.ts
@@ -4,46 +4,53 @@ const nextConfig: NextConfig = {
   // allow google profile pictures and supabase images
   // FIXME: use remotePatterns instead
   images: {
-    domains: ["lh3.googleusercontent.com", (process.env.NEXT_PUBLIC_SUPABASE_URL as string).replace(/https?:\/\//, "")],
-  },
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'X-Content-Type-Options',
-            value: 'nosniff',
-          },
-          {
-            key: 'X-Frame-Options',
-            value: 'DENY',
-          },
-          {
-            key: 'Referrer-Policy',
-            value: 'strict-origin-when-cross-origin',
-          },
-        ],
-      },
-      {
-        source: '/sw.js',
-        headers: [
-          {
-            key: 'Content-Type',
-            value: 'application/javascript; charset=utf-8',
-          },
-          {
-            key: 'Cache-Control',
-            value: 'no-cache, no-store, must-revalidate',
-          },
-          {
-            key: 'Content-Security-Policy',
-            value: "default-src 'self'; script-src 'self'",
-          },
-        ],
-      },
-    ]
+    domains: [
+      "lh3.googleusercontent.com",
+      (process.env.NEXT_PUBLIC_SUPABASE_URL as string).replace(
+        /https?:\/\//,
+        ""
+      ),
+    ],
   },
 };
+
+export async function headers() {
+  return [
+    {
+      source: "/(.*)",
+      headers: [
+        {
+          key: "X-Content-Type-Options",
+          value: "nosniff",
+        },
+        {
+          key: "X-Frame-Options",
+          value: "DENY",
+        },
+        {
+          key: "Referrer-Policy",
+          value: "strict-origin-when-cross-origin",
+        },
+      ],
+    },
+    {
+      source: "/sw.js",
+      headers: [
+        {
+          key: "Content-Type",
+          value: "application/javascript; charset=utf-8",
+        },
+        {
+          key: "Cache-Control",
+          value: "no-cache, no-store, must-revalidate",
+        },
+        {
+          key: "Content-Security-Policy",
+          value: "default-src 'self'; script-src 'self'",
+        },
+      ],
+    },
+  ];
+}
 
 export default nextConfig;
