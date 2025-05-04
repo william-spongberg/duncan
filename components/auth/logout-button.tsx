b84def0cb3a17aedb20a16e9f83333640f2ed132
William Spongberg <william.spongberg@outlook.com>
2025-05-04 16:44:15 +1000

    Purge cache on logout to ensure auth

--- components/auth/logout-button.tsx
+++ components/auth/logout-button.tsx
@@ -3,15 +3,27 @@
 import { createClient } from "@/lib/auth/client";
 import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
+import { clear } from "idb-keyval";
 
 export function LogoutButton() {
   const router = useRouter();
 
   const logout = async () => {
+    // purge all cache
+    await purgeCache();
+
+    // logout
     const supabase = createClient();
     await supabase.auth.signOut();
+
+    // return to login page
     router.push("/auth/login");
   };
 
+  const purgeCache = async () => {
+    await clear();
+  }
+
   return <Button onClick={logout}>Logout</Button>;
 }
+
